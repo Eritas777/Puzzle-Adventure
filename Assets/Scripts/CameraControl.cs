f5713b08f0d414b8b628412e7b81d4635f645cb9
marikdead <skvortsov_mark@mail.ru>
2023-03-14 19:14:05 +0700

    Add character

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float smoothSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,new Vector3(target.position.x,target.position.y,transform.position.z),smoothSpeed *Time.deltaTime);
+    }
+}
